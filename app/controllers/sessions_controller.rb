fb69b0e97e2409743d61f366ea036ed5fc217640
Jennifer <jennifersoden@hotmail.com>
2016-09-15 11:54:15 -0600

    Allow user to logout

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,8 +6,8 @@ class SessionsController < ApplicationController
     redirect_to root_path
   end
 
-  # def destroy
-  #   session.clear
-  #   redirect_to root_path
-  # end
+  def destroy
+    session.clear
+    redirect_to root_path
+  end
 end
